332e427c54f743009030dc6654162d642475fc8a
DESKTOP-KS932QI\madcat <madcat.samara@gmail.com>
2018-04-22 16:41:44 +0400

    Chapter 3 Priority queue

---
+++ Ch03/PriorityQueue.Test/Tests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PriorityQueue;
+
+namespace PriorityQueue.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Insert_ValidParamsPassed_Success()
+        {
+
+            IPriorityQueue<int> priorityQueue = new BinaryHeapPriorityQueue<int>(5, (x, y) => y.CompareTo(x));
+            
+            priorityQueue.Insert(5);
+            priorityQueue.Insert(4);
+            priorityQueue.Insert(3);
+            priorityQueue.Insert(2);
+            priorityQueue.Insert(1);
+            
+            Assert.IsTrue(priorityQueue.IsFull());
+        }
+        
+        [Test]
+        public void Insert_QueueOverflow_Failure()
+        {
+
+            IPriorityQueue<int> priorityQueue = new BinaryHeapPriorityQueue<int>(4, (x, y) => y.CompareTo(x));
+            
+            priorityQueue.Insert(5);
+            priorityQueue.Insert(4);
+            priorityQueue.Insert(3);
+            priorityQueue.Insert(2);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                priorityQueue.Insert(1);
+            });
+        }
+        
+        [Test]
+        public void Extract_ValidParamsPassed_Success()
+        {
+
+            IPriorityQueue<int> priorityQueue = new BinaryHeapPriorityQueue<int>(5, (x, y) => -x.CompareTo(y));
+            
+            priorityQueue.Insert(5);
+            priorityQueue.Insert(4);
+            priorityQueue.Insert(3);
+            priorityQueue.Insert(2);
+            priorityQueue.Insert(1);
+            
+            var v1 = priorityQueue.Extract();
+            var v2 = priorityQueue.Extract();
+            var v3 = priorityQueue.Extract();
+            var v4 = priorityQueue.Extract();
+            var v5 = priorityQueue.Extract();
+
+            Assert.AreEqual(v1, 1);
+            Assert.AreEqual(v2, 2);
+            Assert.AreEqual(v3, 3);
+            Assert.AreEqual(v4, 4);
+            Assert.AreEqual(v5, 5);
+        }
+        
+        [Test]
+        public void Peek_ValidParamsPassed_Success()
+        {
+
+            IPriorityQueue<int> priorityQueue = new BinaryHeapPriorityQueue<int>(5, (x, y) => -x.CompareTo(y));
+            
+            priorityQueue.Insert(5);
+            priorityQueue.Insert(4);
+            priorityQueue.Insert(3);
+            priorityQueue.Insert(2);
+            priorityQueue.Insert(1);
+
+            var v1 = priorityQueue.Peek();
+
+            Assert.AreEqual(v1, 1);
+        }
+        
+        [Test]
+        public void GetSize_ValidParamsPassed_Success()
+        {
+
+            IPriorityQueue<int> priorityQueue = new BinaryHeapPriorityQueue<int>(5, (x, y) => -x.CompareTo(y));
+            
+            priorityQueue.Insert(5);
+            priorityQueue.Insert(4);
+            priorityQueue.Insert(3);
+
+            var size = priorityQueue.GetSize();
+
+            Assert.AreEqual(size, 3);
+        }
+        
+        [Test]
+        public void IsEmpty_ValidParamsPassed_Success()
+        {
+
+            IPriorityQueue<int> priorityQueue = new BinaryHeapPriorityQueue<int>(5, (x, y) => x.CompareTo(y));
+            
+            Assert.IsTrue(priorityQueue.IsEmpty());
+        }
+        
+        
+    }
+}
---
+++ Ch03/PriorityQueue/BinaryHeapPriorityQueue.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PriorityQueue
+{
+    public interface IPriorityQueue<T>
+    {
+        void Insert(T item);
+
+        T Extract();
+
+        T Peek();
+
+        int GetSize();
+
+        bool IsFull();
+
+        bool IsEmpty();
+    }
+
+    public class BinaryHeapPriorityQueue<T> : IPriorityQueue<T>
+    {
+        
+        private readonly Func<T, T, int> _comparer;
+        private readonly T[] _data; // heap-ordered complete binary tree
+        private int _n = 0; // zero unused 
+        
+        public BinaryHeapPriorityQueue(int queueSize, Func<T, T, int> comparer)
+        {
+            _data = new T[queueSize+1];
+            _comparer = comparer;
+        }
+        
+        public void Insert(T item)
+        {
+            if (IsFull())
+                throw new InvalidOperationException("Queue is full!");
+
+            _data[++_n] = item;
+            Swim(_n);
+        }
+
+        public T Extract()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty!");
+
+            T item = _data[1];
+            Swap(1, _n--);
+            _data[_n + 1] = default(T);
+            Sink(1);
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            return _data[1];
+        }
+
+        public int GetSize()
+        {
+            return _n;
+        }
+        
+        public bool IsFull()
+        {
+            return _n == _data.Length - 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return _n == 0;
+        }
+
+        private void Swim(int k)
+        {
+            // check for out-of-bounds array
+            while (k > 1 && Less(k / 2, k)) 
+            {
+                Swap(k / 2, k);
+                k = k / 2;
+            }
+        }
+
+        private void Sink(int k)
+        {
+            while (2*k <= _n) // check for out-of-bounds array
+            {
+                int j = 2*k; // descendant index
+                if (j < _n && Less(j, j + 1)) // seek for larger descendant to perform exchange 
+                    j++;
+                
+                // procedure is stopped as the current element that
+                // we wanted to sift down has become larger than the largest descendant,
+                // therefore we have found the proper place
+                if (!Less(k, j)) 
+                    break;
+                
+                // procedure is not stopped; go further
+                Swap(k, j);
+                k = j;
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            var t = _data[i];
+            _data[i] = _data[j];
+            _data[j] = t;
+        }
+
+        private bool Less(int i, int j)
+        {
+            return _comparer(_data[i], _data[j]) < 0;
+        }
+    }
+}
